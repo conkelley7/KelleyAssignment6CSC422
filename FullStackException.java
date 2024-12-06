2390eb2f0fbd73f25b18b3e0b75c5197a7c309c3
conkelley7 <conkelley7@gmail.com>
2024-12-06 14:55:46 -0600

    Create FullStackException.java

---
+++ FullStackException.java
@@ -0,0 +1,11 @@
+package com.ckelley.kelley422week6;
+
+/**
+ *
+ * @author conke
+ */
+public class FullStackException extends RuntimeException {
+    public FullStackException(String message) {
+        super(message);
+    }
+}
