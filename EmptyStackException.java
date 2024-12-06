cdb98867fbc46f08648dbb63076289e38276c057
conkelley7 <conkelley7@gmail.com>
2024-12-06 14:55:11 -0600

    Create EmptyStackException.java

---
+++ EmptyStackException.java
@@ -0,0 +1,11 @@
+package com.ckelley.kelley422week6;
+
+/**
+ *
+ * @author conke
+ */
+public class EmptyStackException extends RuntimeException {
+    public EmptyStackException(String message) {
+        super(message);
+    }
+}
